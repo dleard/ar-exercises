aa359f7a227e1b9723d7c1b00188d856b003e209
Dylan Leard <dleard@uvic.ca>
2019-02-19 20:30:18 +0000

    ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "---Sum of all annual_revenue---"
+puts Store.sum(:annual_revenue)
+puts "---Average annual_revenue---"
+puts Store.average(:annual_revenue)
+puts "---# of stores generating 1000000+ annual_revenue"
+puts Store.where("annual_revenue > 1000000").count
